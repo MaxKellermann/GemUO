1580a326801f7af7734a89ce3b6458f6cb59d968
Max Kellermann <max@duempel.org>
2012-09-02 18:10:18 +0200

    gemuo.translate: add event on_localized_system_message()

--- src/gemuo/translate.py
+++ src/gemuo/translate.py
@@ -27,3 +27,7 @@ class Translate(Engine):
             if packet.type == 0 and packet.serial == 0xffffffff and \
                packet.name == 'System':
                 self._signal('on_system_message', packet.text)
+        elif isinstance(packet, p.LocalizedMessage):
+            if packet.type == 0 and packet.serial == 0xffffffff and \
+               packet.name == 'System':
+                self._signal('on_localized_system_message', packet.text)
