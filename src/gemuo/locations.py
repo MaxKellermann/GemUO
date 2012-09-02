6bcbaa4e75cf6c7207c4bc86da49e0f4892728f8
Max Kellermann <max@duempel.org>
2012-09-02 17:57:40 +0200

    gemuo.locations: add the Rel Por bank

--- src/gemuo/locations.py
+++ src/gemuo/locations.py
@@ -17,7 +17,11 @@ BRITAIN_EAST_BANK = (1646,1598, 1646,1616, 1720,1580)
 MINOC_BANK = (2493,541, 2513,561, 2500,570)
 MOONGLOW_BANK = (4461,1150, 4481,1176, 4442,1200)
 
-def nearest_bank(world, position):
+def is_rel_por(world):
+    """Is this the RelPor freeshard?"""
+    return world.map_width == 7168 and world.map_height == 4096
+
+def nearest_felucca_bank(world, position):
     if position.x < 1500:
         #BANK = BRITAIN_WEST_BANK
         # XXX implement
@@ -28,3 +32,9 @@ def nearest_bank(world, position):
         return MOONGLOW_BANK
     else:
         return MINOC_BANK
+
+def nearest_bank(world, position):
+    if is_rel_por(world):
+        return (1335,1370, 1349,1381, 1281,1395)
+    else:
+        return nearest_felucca_bank(world, position)
