48434eddaf32d41e2dcd596f999d39332755b38f
Kai Sassmannshausen <uo@sassie.org>
2012-09-07 12:28:12 +0200

    added: anwer_gump and its engine to test the gump response

---
+++ src/answer_gump.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python#
+#
+#  GemUO
+#
+#  (c) 2005-2010 Max Kellermann <max@duempel.org>
+#                Kai Sassmannshausen <kai@sassie.org>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 of the License.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+
+from uo.skills import *
+from gemuo.simple import simple_run
+from gemuo.engine.training import SkillTraining
+from gemuo.engine.messages import PrintMessages
+from gemuo.engine.guards import Guards
+from gemuo.engine.watch import Watch
+from gemuo.engine.answer_gump import ShowGump, AnswerGump
+
+def run(client):
+    PrintMessages(client)
+    Watch(client)
+
+    return AnswerGump(client)
+
+simple_run(run)
+
