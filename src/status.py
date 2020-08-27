MaxKellermann/GemUO

commit 0c0e95c75dac43f7517afb748cd7fd51ab4f19c4

migrate to Python 3
Python 2 is unmaintained.  Let's go 3!

src/status.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 from twisted.internet import defer
 from uo.skills import *
@@ -8,31 +8,31 @@ from gemuo.engine.player import QuerySkills, QueryStats
 
 def status(result, client):
     player = client.world.player
-    print "Name:", "\t", player.name
+    print("Name:", "\t", player.name)
     if player.position is not None:
-        print "Pos:", "\t", player.position
-    print "Serial: ", player.serial
-    print "Body:", "\t", player.body
-    print "Stats:", "\t", zip(("Str", "Dex", "Int"), player.stats)
+        print("Pos:", "\t", player.position)
+    print("Serial: ", player.serial)
+    print("Body:", "\t", player.body)
+    print("Stats:", "\t", list(zip(("Str", "Dex", "Int"), player.stats)))
     if player.hits is not None:
-        print "Hits:", "\t", player.hits.value, "/", player.hits.limit
+        print("Hits:", "\t", player.hits.value, "/", player.hits.limit)
     if player.mana is not None:
-        print "Mana:", "\t", player.mana.value, "/", player.mana.limit
+        print("Mana:", "\t", player.mana.value, "/", player.mana.limit)
     if player.stamina is not None:
-        print "Stam:", "\t", player.stamina.value, "/", player.stamina.limit
+        prin(("Stam:", "\t", player.stamina.value, "/", player.stamina.limit)
 
-    print "Skills:"
-    skills = filter(lambda x: x.base > 0, player.skills.itervalues())
-    skills.sort(lambda a, b: cmp(a.base, b.base), reverse=True)
+    print("Skills:")
+    skills = [x for x in iter(list(player.skills.values())) if x.base > 0]
+    skills.sort(key=lambda x: x.base, reverse=True)
     total = 0
     for x in skills:
-        print "\t", SKILL_NAMES[x.id], x.base / 10.0, LOCK_NAMES[x.lock]
+        print("\t", SKILL_NAMES[x.id], x.base / 10.0, LOCK_NAMES[x.lock])
         total += x.base
-    print "\t", "Total", total / 10.0
+    print("\t", "Total", total / 10.0)
 
-    print "Equipped:"
+    print("Equipped:")
     for x in client.world.items_in(client.world.player):
-        print "\t", x
+        print("\t", x)
         pass
 
 def run(client):

commit 1be2e634bc360e624790924d2671894084a98363

status.py: fix typo
Fallout from commit 0c0e95c75

src/status.py
@@ -19,7 +19,7 @@ def status(result, client):
     if player.mana is not None:
         print("Mana:", "\t", player.mana.value, "/", player.mana.limit)
     if player.stamina is not None:
-        prin(("Stam:", "\t", player.stamina.value, "/", player.stamina.limit)
+        print("Stam:", "\t", player.stamina.value, "/", player.stamina.limit)
 
     print("Skills:")
     skills = [x for x in iter(list(player.skills.values())) if x.base > 0]